3d0c43e68acf3d6abb226feddc78e51b7b7d07c9
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-10-29 00:51:17 +0700

    setup integrate Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,6 +46,8 @@ dependencies {
     implementation(libs.recyclerview)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-analytics")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
